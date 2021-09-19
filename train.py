a4373ee650eeb479d13c3ad73183893d87ec73f9
SSHH007 <33293050+SSHH007@users.noreply.github.com>
2021-09-19 15:49:03 -0400

    Add files via upload

---
+++ train.py
@@ -0,0 +1,182 @@
+import os
+import numpy as np
+import random
+import time
+import glob
+from Bio import SeqIO
+from pybedtools import BedTool
+from keras.models import load_model
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.optimizers import Adadelta
+from sklearn import metrics
+import h5py
+
+INPUT_LENGTH = 1000
+EPOCH = 200
+BATCH_SIZE = 64
+
+def run_model(data, model, save_dir):
+
+    weights_file = os.path.join(save_dir, "model_weights.hdf5")
+    model_file = os.path.join(save_dir, "single_model.hdf5")
+    model.save(model_file)
+
+    # Adadelta is recommended to be used with default values
+    opt = Adadelta()
+
+    # parallel_model = ModelMGPU(model, gpus=GPUS)
+    parallel_model = model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    X_train = data["train_data"]
+    Y_train = data["train_labels"]
+    X_validation = data["val_data"]
+    Y_validation = data["val_labels"]
+    X_test = data["test_data"]
+    Y_test = data["test_labels"]
+
+    from keras.utils.np_utils import to_categorical
+    Y_train = to_categorical(Y_train, num_classes=None)
+    Y_test = to_categorical(Y_test, num_classes=None)
+    Y_validation = to_categorical(Y_validation, num_classes=None)
+
+    _callbacks = []
+    checkpointer = ModelCheckpoint(filepath=weights_file, verbose=1, save_best_only=True)
+    _callbacks.append(checkpointer)
+    earlystopper = EarlyStopping(monitor="val_loss", patience=10, verbose=1)
+    _callbacks.append(earlystopper)
+
+    parallel_model.fit(X_train,
+                       Y_train,
+                       batch_size=BATCH_SIZE * GPUS,
+                       epochs=EPOCH,
+                       validation_data=(X_validation, Y_validation),
+                       shuffle=True,
+                       callbacks=_callbacks, verbose=1)
+
+    Y_pred = parallel_model.predict(X_test)
+
+    auc1 = metrics.roc_auc_score(Y_test[:,1], Y_pred[:,1])
+    auc2 = metrics.roc_auc_score(Y_test[:,2], Y_pred[:,2])
+
+    with open(os.path.join(save_dir, "auc.txt"), "w") as of:
+        of.write("enhancer AUC: %f\n" % auc2)
+        of.write("silencer AUC: %f\n" % auc1)
+
+    [fprs, tprs, thrs] = metrics.roc_curve(Y_test[:,1], Y_pred[:, 1])
+    sort_ix = np.argsort(np.abs(fprs - 0.1))
+    fpr10_thr = thrs[sort_ix[0]]
+    sort_ix = np.argsort(np.abs(fprs - 0.05))
+    fpr5_thr = thrs[sort_ix[0]]
+    sort_ix = np.argsort(np.abs(fprs - 0.03))
+    fpr3_thr = thrs[sort_ix[0]]
+    sort_ix = np.argsort(np.abs(fprs - 0.01))
+    fpr1_thr = thrs[sort_ix[0]]
+
+    [fprs, tprs, thrs] = metrics.roc_curve(Y_test[:,2], Y_pred[:, 2])
+    sort_ix = np.argsort(np.abs(fprs - 0.1))
+    fpr10_thre = thrs[sort_ix[0]]
+    sort_ix = np.argsort(np.abs(fprs - 0.05))
+    fpr5_thre = thrs[sort_ix[0]]
+    sort_ix = np.argsort(np.abs(fprs - 0.03))
+    fpr3_thre = thrs[sort_ix[0]]
+    sort_ix = np.argsort(np.abs(fprs - 0.01))
+    fpr1_thre = thrs[sort_ix[0]]
+
+    with open(os.path.join(save_dir, "fpr_threshold_scores.txt"), "w") as of:
+        of.write("silencer 10 \t %f\n" % fpr10_thr)
+        of.write("5 \t %f\n" % fpr5_thr)
+        of.write("3 \t %f\n" % fpr3_thr)
+        of.write("1 \t %f\n\n" % fpr1_thr)
+        of.write("enhancer 10 \t %f\n" % fpr10_thre)
+        of.write("5 \t %f\n" % fpr5_thre)
+        of.write("3 \t %f\n" % fpr3_thre)
+        of.write("1 \t %f\n" % fpr1_thre)
+
+def run_model(X, model):
+
+    Y = model.predict(X)
+    return Y
+
+
+def load_dataset(ENf, SLf, neg):
+
+    print("reading enhancers...")
+    data = {}
+    with h5py.File(ENf, "r") as inf:
+        for _key in inf:
+            data[_key] = inf[_key][()]
+
+    data["train_data"] = data["train_data"][..., np.newaxis]
+    data["test_data"] = data["test_data"][..., np.newaxis]
+    data["val_data"] = data["val_data"][..., np.newaxis]
+
+    data["train_labels"] = np.zeros(data["train_data"].shape[0])+2
+    data["test_labels"] = np.zeros(data["test_data"].shape[0])+2
+    data["val_labels"] = np.zeros(data["val_data"].shape[0])+2
+
+    print("reading silencers...")
+    SLdata = {}
+    with h5py.File(SLf, "r") as inf:
+        for _key in inf:
+            SLdata[_key] = inf[_key][()]
+
+    xi = SLdata["train_data"][..., np.newaxis]
+    data["train_data"] = np.vstack((data["train_data"], xi))
+    data["test_data"] = np.vstack((data["test_data"], SLdata["test_data"][..., np.newaxis]))
+    data["val_data"] = np.vstack((data["val_data"], SLdata["val_data"][..., np.newaxis]))
+    data["train_labels"] = np.concatenate((data["train_labels"], np.zeros(SLdata["train_data"].shape[0])+1))
+    data["test_labels"] = np.concatenate((data["test_labels"], np.zeros(SLdata["test_data"].shape[0])+1))
+    data["val_labels"] = np.concatenate((data["val_labels"], np.zeros(SLdata["val_data"].shape[0])+1))
+
+
+    print("reading controls...")
+    SLdata = {}
+    with h5py.File(neg, "r") as inf:
+        for _key in inf: 
+            SLdata[_key] = inf[_key][()]
+    
+    xi = SLdata["train_data"][..., np.newaxis]
+    data["train_data"] = np.vstack((data["train_data"], xi))
+    data["test_data"] = np.vstack((data["test_data"], SLdata["test_data"][..., np.newaxis]))
+    data["val_data"] = np.vstack((data["val_data"], SLdata["val_data"][..., np.newaxis]))
+    data["train_labels"] = np.concatenate((data["train_labels"], np.zeros(SLdata["train_data"].shape[0])))
+    data["test_labels"] = np.concatenate((data["test_labels"], np.zeros(SLdata["test_data"].shape[0])))
+    data["val_labels"] = np.concatenate((data["val_labels"], np.zeros(SLdata["val_data"].shape[0])))
+
+    print("training enhancers:", (data["train_labels"]==2).sum())
+    print("training silencer:", (data["train_labels"]==1).sum())
+    print("training controls:", (data["train_labels"]==0).sum())
+
+    return data
+
+
+def train_model(ENf,SLf, negf, results_dir):
+
+    phase_two_model_file = WORK_DIR + "/source_files/model.hdf5"
+    model = load_model(phase_two_model_file)
+   
+    if not os.path.exists(ENf):
+        print("no data file"+ENf)
+        exit()
+    if not os.path.exists(SLf):
+        print("no data file"+SLf)
+        exit()
+    if not os.path.exists(negf):
+        print("no data file"+negf)
+        exit()
+    if not os.path.exists(results_dir):
+        os.mkdir(results_dir)
+    
+    data = load_dataset(ENf,SLf,negf)
+    run_model(data, model, results_dir)
+
+
+if __name__ == "__main__":
+
+    import sys
+    enhancer_file = sys.argv[1]
+    silencer_file = sys.argv[2]
+    neg_file = sys.argv[3]
+    results_dir = sys.argv[4]
+    train_model(enhancer_file, silencer_file, neg_file, results_dir)
